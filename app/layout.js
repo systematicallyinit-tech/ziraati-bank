1d06e5ce5b545b2357bf89eb17caaa815f66d01e
Elon Gustavo <systematicallyinit@gmail.com>
2026-09-16 15:13:59 +0100

    Fix typo in bank title metadata

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "Bir Bankadan Daha Fazlası | Ziraati Bankasi",
+  title: "Bir Bankadan Daha Fazlası | Ziraat Bankası",
   description:
     "Bankacılık ürünleri ve hizmetleri hakkında bilgi verebiliriz. Kredi koşullarını hesaplayabilir ve başvurunuzu işleme koyabiliriz. Ayrıca mevduat getirilerini de hesaplayabiliriz.",
   keywords:
