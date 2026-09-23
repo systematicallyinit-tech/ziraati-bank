b2144928a9fe088d9c25a092365b9a1b7ea5f99f
Elon Gustavo <systematicallyinit@gmail.com>
2026-09-23 21:16:25 +0100

    Add LiveChat component to layout

--- app/layout.js
+++ app/layout.js
@@ -103,7 +103,7 @@ export default function RootLayout({ children }) {
         <Providers>
           {children}
         </Providers>
-        
+        <LiveChat />
       </body>
     </html>
   );
