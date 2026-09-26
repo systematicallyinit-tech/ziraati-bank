be45a9a67eb55495a2d70489d92038df725a1a8f
Elon Gustavo <systematicallyinit@gmail.com>
2026-09-26 09:45:01 +0100

    Simplify ThemeProvider props
    Removed unnecessary props from ThemeProvider.

--- app/providers.js
+++ app/providers.js
@@ -5,9 +5,7 @@ import { ThemeProvider } from "next-themes";
 export default function Providers({ children }) {
   return (
     <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
+      
     >
       {children}
     </ThemeProvider>
